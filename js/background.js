f3fb6df151f97b7e0df72d9342327286c0633a06
capoaira <capoaira@web.de>
2024-08-06 13:36:13 +0200

    fix context menu
    fixes #4

--- js/background.js
+++ js/background.js
@@ -28,7 +28,7 @@ const copy2clipboard = (text) => {
 };
 
 /**
- * Sends a massage to to
+ * Sends a message to a tab
  * @param {?} tabId
  * @param {string} msg
  */
@@ -42,7 +42,7 @@ const sendMessage = (tabId, msg) => {
 };
 
 /**
- * Handels Messages from the content script
+ * Handles messages from the content script
  *
  * @param {?} request
  * @param {?} _sender
@@ -52,129 +52,139 @@ const sendMessage = (tabId, msg) => {
 const handleMessage = (request, _sender, _sendResponse) => {
   GCW.selection = request.selection;
   if (request.do === 'contextmenu') {
-    if (request.selection.match(/(GC|TB|BM|GT)[A-Z0-9]+/i)) {
-      browser.contextMenus.create({
-        id: 'open_with_gcw',
-        title: GCW.i18n('open_gcw'),
-        contexts: ['selection'],
-        parentId: 'gcw',
-      });
-      browser.contextMenus.refresh();
-    }
-    const parentId = request.selection.match(/(GC|TB|BM|GT)[A-Z0-9]+/i)
-      ? 'open_with_gcw'
-      : 'gcw';
-    console.log(parentId);
-    let subMenus = {};
-    // Create Menuentries for tools
-    for (const tool of GCW.tools) {
-      let [toolName, val] = Object.entries(tool)[0];
-      toolName = toolName.split('/')[1];
-      // Test if there is a supercategory (like base or rotation)
-      if (toolName.includes('_')) {
-        const superCat = toolName.split('_')[0];
-        if (!subMenus[superCat]) {
-          // No? => Create
-          const sc = browser.contextMenus.create({
-            id: superCat,
-            title: GCW.i18n(superCat),
+    // Remove all existing menu entries
+    browser.contextMenus
+      .removeAll()
+      .then(() => {
+        // Create the main context menu
+        browser.contextMenus.create({
+          id: 'gcw',
+          title: GCW.i18n('extension_name'),
+          contexts: ['selection'],
+        });
+
+        if (request.selection.match(/(GC|TB|BM|GT)[A-Z0-9]+/i)) {
+          browser.contextMenus.create({
+            id: 'open_with_gcw',
+            title: GCW.i18n('open_gcw'),
             contexts: ['selection'],
-            parentId: parentId,
+            parentId: 'gcw',
           });
-          browser.contextMenus.refresh();
-          subMenus[superCat] = sc;
         }
-      }
-      // Create Tool entry
-      const entry = browser.contextMenus.create({
-        id: `open_gcw-${toolName}`,
-        title: GCW.i18n(toolName),
-        contexts: ['selection'],
-        parentId: toolName.includes('_') ? toolName.split('_')[0] : parentId,
-      });
-      browser.contextMenus.refresh();
-      const params = val['get']['parameters'];
-      if (!params) {
-        console.error('no params', toolName);
-        continue;
-      }
-      for (const param of params) {
-        switch (param.name) {
-          case 'mode':
-          case 'lang':
-            for (const name of param.schema.enum) {
-              browser.contextMenus.create({
-                id: `open_gcw-${toolName}-${name}`,
-                title: GCW.i18n(name),
+
+        const parentId = request.selection.match(/(GC|TB|BM|GT)[A-Z0-9]+/i)
+          ? 'open_with_gcw'
+          : 'gcw';
+        let subMenus = {};
+
+        // Create Menuentries for tools
+        for (const tool of GCW.tools) {
+          let [toolName, val] = Object.entries(tool)[0];
+          toolName = toolName.split('/')[1];
+
+          // Test if there is a supercategory (like base or rotation)
+          if (toolName.includes('_')) {
+            const superCat = toolName.split('_')[0];
+            if (!subMenus[superCat]) {
+              // No? => Create
+              const sc = browser.contextMenus.create({
+                id: superCat,
+                title: GCW.i18n(superCat),
                 contexts: ['selection'],
-                parentId: entry,
+                parentId: parentId,
               });
-              browser.contextMenus.refresh();
+              subMenus[superCat] = sc;
             }
+          }
+
+          // Create Tool entry
+          const entry = browser.contextMenus.create({
+            id: `open_gcw-${toolName}`,
+            title: GCW.i18n(toolName),
+            contexts: ['selection'],
+            parentId: toolName.includes('_')
+              ? toolName.split('_')[0]
+              : parentId,
+          });
+
+          const params = val['get']['parameters'];
+          if (!params) {
+            console.error('no params', toolName);
+            continue;
+          }
+
+          for (const param of params) {
+            switch (param.name) {
+              case 'mode':
+              case 'lang':
+                for (const name of param.schema.enum) {
+                  browser.contextMenus.create({
+                    id: `open_gcw-${toolName}-${name}`,
+                    title: GCW.i18n(name),
+                    contexts: ['selection'],
+                    parentId: entry,
+                  });
+                }
+            }
+          }
         }
-      }
-    }
-    // Open Caches, TBs, Bookmarks and GeoTours
-    if (request.selection.match(/(GC|TB|BM|GT)[A-Z0-9]+/i)) {
-      let type = 'gc';
-      if (request.selection.match(/TB[A-Z0-9]+/i)) type = 'tb';
-      if (request.selection.match(/BM[A-Z0-9]+/i)) type = 'bm';
-      if (request.selection.match(/GT[A-Z0-9]+/i)) type = 'gt';
-      GCW.code_for_link = request.selection.match(
-        /(GC|TB|BM|GT)[A-Z0-9]+\b/i
-      )[0];
-      browser.contextMenus.create({
-        id: 'open' + GCW.open_id,
-        title: GCW.i18n('open_' + type),
-        contexts: ['selection'],
-        parentId: 'gcw',
-      });
-      browser.contextMenus.refresh();
-    } else {
-      // Remove option to open caches, TBs, Bookmarks or GeoTours (If it was added)
-      if (GCW.code_for_link !== false) {
-        browser.contextMenus.remove('open' + GCW.open_id);
-        browser.contextMenus.remove('open_with_gcw');
-        GCW.open_id++;
-        GCW.code_for_link = false;
+
+        // Open Caches, TBs, Bookmarks and GeoTours
+        if (request.selection.match(/(GC|TB|BM|GT)[A-Z0-9]+/i)) {
+          let type = 'gc';
+          if (request.selection.match(/TB[A-Z0-9]+/i)) type = 'tb';
+          if (request.selection.match(/BM[A-Z0-9]+/i)) type = 'bm';
+          if (request.selection.match(/GT[A-Z0-9]+/i)) type = 'gt';
+          GCW.code_for_link = request.selection.match(
+            /(GC|TB|BM|GT)[A-Z0-9]+\b/i
+          )[0];
+          browser.contextMenus.create({
+            id: 'open' + GCW.open_id,
+            title: GCW.i18n('open_' + type),
+            contexts: ['selection'],
+            parentId: 'gcw',
+          });
+        } else {
+          // Remove option to open caches, TBs, Bookmarks or GeoTours (If it was added)
+          if (GCW.code_for_link !== false) {
+            browser.contextMenus.remove('open' + GCW.open_id);
+            browser.contextMenus.remove('open_with_gcw');
+            GCW.open_id++;
+            GCW.code_for_link = false;
+          }
+        }
         browser.contextMenus.refresh();
-      }
-    }
+      })
+      .catch(onError);
+
     return Promise.resolve({ response: 'context menu' });
   } else if (request.do === 'openTab') {
     browser.storage.local.set({ analyzeData: request.payload });
     browser.tabs.create({ url: request.url });
     return Promise.resolve({ response: 'open analyze' });
   }
-  browser.contextMenus.refresh();
   return Promise.resolve({ response: 'did nothing' });
 };
 
 const main = async () => {
-  // Create GCW context menu entry
-  browser.contextMenus.create({
-    id: 'gcw',
-    title: GCW.i18n('extension_name'),
-    contexts: ['selection'],
-  });
-  browser.contextMenus.refresh();
-
   // Links to Caches, TBs, Bookmarks or GeoTours
   GCW.code_for_link = false;
   GCW.open_id = 1;
+
   // Links to online Tools
   const jsonFilePath = browser.runtime.getURL('data/tools.json');
-  // Funktion zum Laden der JSON-Datei
+  // Function to load the JSON file
   const loadJSONFile = async (filePath) => {
     try {
       const response = await fetch(filePath);
       const json = await response.json();
-      console.info('JSON-Datei loaded:', json);
+      console.info('JSON file loaded:', json);
       return json;
     } catch (error) {
       console.error('Error loading JSON file:', error);
     }
   };
+
   GCW.tools = await loadJSONFile(jsonFilePath);
   GCW.selection;
 
@@ -192,6 +202,7 @@ const main = async () => {
       }
       sendMessage(tab.id, { do: 'openGCW', tool: options[1], params: params });
     }
+
     if (info.menuItemId.match(/open\d+/i)) {
       // Open Cache, TB, Bookmark or GeoTour
       sendMessage(tab.id, {
@@ -199,13 +210,13 @@ const main = async () => {
         href: 'https://coord.info/' + GCW.code_for_link,
       });
     }
+
     // Remove option to open caches, TBs, Bookmarks or GeoTours (If it was added)
     if (GCW.code_for_link !== false) {
       browser.contextMenus.remove('open' + GCW.open_id);
       browser.contextMenus.remove('open_with_gcw');
       GCW.open_id++;
       GCW.code_for_link = false;
-      browser.contextMenus.refresh();
     }
   });
 
